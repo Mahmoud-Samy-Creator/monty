713ef7b42ee96d0c77c9b157eb5d78922e055c7b
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-08-25 02:36:13 +0300

    support pchar opcode

--- monty.h
+++ monty.h
@@ -92,6 +92,7 @@ void stack_mod(stack_t **stack_top, UNUSED unsigned int line_number);
 
 /*          stack_2.c          */
 void stack_rotl(stack_t **stack_top, UNUSED unsigned int line_number);
+void stack_pchar(stack_t **stack_top, unsigned int line_number);
 
 
 /*          strdup.c          */
--- stack_2.c
+++ stack_2.c
@@ -24,3 +24,26 @@ void stack_rotl(stack_t **stack_top, UNUSED unsigned int line_number)
 	*stack_top = (*stack_top)->prev;
 	free(curr);
 }
+
+/**
+ * stack_pchar - A function to print the last element of stack as a char
+ * @stack: ptr ot ptr of the stack elements
+ * @line_number: The line counter
+ * Return: void
+ */
+void stack_pchar(stack_t **stack_top, unsigned int line_number)
+{
+  if (!stack_top || !(*stack_top))
+  {
+    fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+    free_ctx();
+    exit(EXIT_FAILURE);
+  }
+  else if ((*stack_top)->n < 0 && (*stack_top)->n > 127)
+  {
+    fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
+    free_ctx();
+    exit(EXIT_FAILURE);
+  }
+  fprintf(stdout, "%c\n", (*stack_top)->n);
+}
