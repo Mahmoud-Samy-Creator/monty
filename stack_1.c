d32e93cd47ad26895cec60667da2875ce810b3f0
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-08-25 19:09:20 +0300

    fix errors

--- stack_1.c
+++ stack_1.c
@@ -45,7 +45,7 @@ void stack_pop(stack_t **stack, unsigned int line_number)
 
 	if (stack == NULL || (*stack) == NULL)
 	{
-		fprintf(stderr, "L%u: can't pop an stack empty\n", line_number);
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
 		free_ctx();
 		exit(EXIT_FAILURE);
 	}
